f916517f07ca1e638ec0878dca25262814d9c9a0
Nicola Peduzzi <thenikso@gmail.com>
2012-11-30 14:01:29 +0100

    Added BOCHS configuration file and pre build binary.

--- Makefile.bat
+++ Makefile.bat
@@ -1,6 +1,5 @@
-fasm Stage1\Stage1.asm bin\Stage1
-fasm Stage2\Stage2.asm bin\Stage2
-fasm Stage1\Loaders\Stage1.Loader.FAT32.asm bin\Stage1.Loader.FAT32
-pause Copia in Bochs
-copy bin\Stage1/b + bin\Stage2/b  bin\Bootes.img
-pause
+fasm Stage1\Stage1.asm bin\objects\Stage1
+fasm Stage2\Stage2.asm bin\objects\Stage2
+fasm Stage1\Loaders\Stage1.Loader.FAT32.asm bin\objects\Stage1.Loader.FAT32
+copy bin\objects\Stage1/b + bin\objects\Stage2/b  bin\Bootes.img
+pause Run BOCHS from the bin directory using the provided bochsrc.txt
